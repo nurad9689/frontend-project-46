3ea8f2fce8ac367980f2d72817ccf6c6a86f0dbd
Nurad Namazov <nurad9689@rambler.ru>
2023-11-06 04:36:59 +0300

    update index.js module

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-import fs from 'fs';
 import _ from 'lodash';
-import path from 'node:path';
-
-const resolvePath = (filePath) => (
-  filePath.includes('__fixtures__')
-    ? path.resolve(process.cwd(), filePath)
-    : path.resolve(process.cwd(), `__fixtures__/${filePath}`));
-
-const getParsedObj = (filePath) => {
-  const pathname = resolvePath(filePath);
-  const data = fs.readFileSync(pathname);
-  return JSON.parse(data);
-};
+import parser from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
-  const obj1 = getParsedObj(filepath1);
-  const obj2 = getParsedObj(filepath2);
+  const obj1 = parser(filepath1);
+  const obj2 = parser(filepath2);
 
   const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
-
   const result = keys.reduce((acc, key) => {
     if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
       if (obj1[key] !== obj2[key]) {
@@ -35,7 +22,5 @@ const genDiff = (filepath1, filepath2) => {
 
   return `{\n${result.join('\n')}\n}`;
 };
-
-genDiff('file1.json', 'file2.json');
 
 export default genDiff;
