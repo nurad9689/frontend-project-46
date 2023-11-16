29c0ff16c7eba8cb7f62725625b0a0a62b0c157a
Nurad Namazov <nurad9689@rambler.ru>
2023-11-16 23:59:47 +0300

    update test file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import { describe } from '@jest/globals';
+import { describe, test } from '@jest/globals';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
@@ -16,13 +16,20 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const resultPath = getFixturePath('Stylish.txt');
-const result = fs.readFileSync(resultPath, 'utf-8');
-
 describe.each(cases)('"%s" and "%s" extentions:', (extent1, extent2) => {
   test('Stylish (default)', () => {
+    const resultPath = getFixturePath('Stylish.txt');
+    const result = fs.readFileSync(resultPath, 'utf-8');
     const path1 = getFixturePath(`file1.${extent1}`);
     const path2 = getFixturePath(`file2.${extent2}`);
     expect(genDiff(path1, path2)).toEqual(result);
+  });
+
+  test('Plain', () => {
+    const resultPath = getFixturePath('Plain.txt');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+    const path1 = getFixturePath(`file1.${extent1}`);
+    const path2 = getFixturePath(`file2.${extent2}`);
+    expect(genDiff(path1, path2, 'plain')).toEqual(result);
   });
 });
