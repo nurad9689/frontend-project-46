f418a91e49a2f35a451b84798d5f9fe56a093067
Nurad Namazov <nurad9689@rambler.ru>
2023-11-16 23:15:42 +0300

    create parser file

---
+++ src/parser.js
@@ -0,0 +1,24 @@
+import path from 'node:path';
+import yaml from 'js-yaml';
+import fs from 'fs';
+
+const resolvePath = (filePath) => (
+  filePath.includes('__fixtures__')
+    ? path.resolve(process.cwd(), filePath)
+    : path.resolve(process.cwd(), `__fixtures__/${filePath}`));
+
+const parser = (filepath) => {
+  const extention = path.extname(filepath);
+  const pathname = resolvePath(filepath);
+  const data = fs.readFileSync(pathname);
+
+  if (extention === '.yaml' || extention === '.yml') {
+    return yaml.load(data);
+  }
+  if (extention === '.json' || extention === '.txt') {
+    return JSON.parse(data);
+  }
+  throw new Error(`${extention} is invalid file type`);
+};
+
+export default parser;
