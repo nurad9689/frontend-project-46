3b565c2fe6a8e71235542775a122056badbb3005
Nurad Namazov <nurad9689@rambler.ru>
2023-11-17 00:00:53 +0300

    rename

--- src/formatters/build.js
+++ src/formatters/build.js
@@ -1,9 +1,12 @@
 import stylish from './stylish.js';
+import plain from './plain.js';
 
 const formatter = (tree, style) => {
   const normalize = style.toLowerCase();
   if (normalize === 'stylish') {
     return stylish(tree);
+  } if (normalize === 'plain') {
+    return plain(tree);
   }
   throw new Error(`Invalid type format: ${style}`);
 };
