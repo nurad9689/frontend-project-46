dda2468e5a40a0274929ab0a310472b4fadd3423
Nurad Namazov <nurad9689@rambler.ru>
2023-11-16 23:26:18 +0300

    create new test system

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,26 +1,28 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { describe } from '@jest/globals';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
-const result = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const cases = [
+  ['json', 'json'],
+  ['json', 'yml'],
+  ['yml', 'json'],
+  ['yml', 'yml'],
+];
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('1st test', () => {
-  const extentions = ['json', 'yml'];
-  extentions.forEach((extention1) => extentions.forEach((extention2) => {
-    const path1 = getFixturePath(`file1.${extention1}`);
-    const path2 = getFixturePath(`file2.${extention2}`);
+const resultPath = getFixturePath('Stylish.txt');
+const result = fs.readFileSync(resultPath, 'utf-8');
+
+describe.each(cases)('"%s" and "%s" extentions:', (extent1, extent2) => {
+  test('Stylish (default)', () => {
+    const path1 = getFixturePath(`file1.${extent1}`);
+    const path2 = getFixturePath(`file2.${extent2}`);
     expect(genDiff(path1, path2)).toEqual(result);
-  }));
+  });
 });
