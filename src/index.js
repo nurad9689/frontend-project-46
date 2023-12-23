d78fc284beec917529012886935c832d8e3d7033
Nurad Namazov <nurad9689@rambler.ru>
2023-12-23 19:19:23 +0300

    change logic of parsing data and difference generator functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+import fs from 'fs';
+import path from 'node:path';
 import parser from './parser.js';
 import comparisonRules from './compression.js';
 import formatter from './formatters/index.js';
 
+const resolvePath = (filePath) => (
+  filePath.includes('__fixtures__')
+    ? path.resolve(process.cwd(), filePath)
+    : path.resolve(process.cwd(), `__fixtures__/${filePath}`));
+
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const file1 = parser(filepath1);
-  const file2 = parser(filepath2);
+  const dataFormat1 = path.extname(filepath1).slice(1);
+  const data1 = fs.readFileSync(resolvePath(filepath1));
+  const dataFormat2 = path.extname(filepath2).slice(1);
+  const data2 = fs.readFileSync(resolvePath(filepath2));
+
+  const file1 = parser(data1, dataFormat1);
+  const file2 = parser(data2, dataFormat2);
+
   const comparison = comparisonRules(file1, file2);
   const result = formatter(comparison, formatName);
   return result;
--- src/parser.js
+++ src/parser.js
@@ -1,24 +1,13 @@
-import path from 'node:path';
 import yaml from 'js-yaml';
-import fs from 'fs';
 
-const resolvePath = (filePath) => (
-  filePath.includes('__fixtures__')
-    ? path.resolve(process.cwd(), filePath)
-    : path.resolve(process.cwd(), `__fixtures__/${filePath}`));
-
-const parser = (filepath) => {
-  const extention = path.extname(filepath);
-  const pathname = resolvePath(filepath);
-  const data = fs.readFileSync(pathname);
-
-  if (extention === '.yaml' || extention === '.yml') {
+const parser = (data, dataFormat) => {
+  if (dataFormat === 'yaml' || dataFormat === 'yml') {
     return yaml.load(data);
   }
-  if (extention === '.json' || extention === '.txt') {
+  if (dataFormat === 'json' || dataFormat === 'txt') {
     return JSON.parse(data);
   }
-  throw new Error(`${extention} is invalid file type`);
+  throw new Error(`${dataFormat} is invalid file type`);
 };
 
 export default parser;
